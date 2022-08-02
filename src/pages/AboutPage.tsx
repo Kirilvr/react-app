aaed3e59d22fd874f08f933376a290dcc6768920
Kirilvr <83722123+Kirilvr@users.noreply.github.com>
2022-08-02 19:36:06 +0300

    add ModalContext and Router

---
+++ src/pages/AboutPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+interface IAboutPage {}
+
+export const AboutPage = ({}: IAboutPage) => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+      laboriosam minima, eius in rerum commodi excepturi soluta at molestias est
+      repellat asperiores harum dolore. Amet cum voluptates mollitia quidem eum!
+    </div>
+  );
+};
